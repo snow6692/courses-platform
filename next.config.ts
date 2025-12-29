9a2f65cbf943834e9f5481ec3635697cc0e60691
snow6692 <ahmedha258258@gmail.com>
2025-12-29 17:55:58 +0200

    allow images from google

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "spider-pl.b-cdn.net",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
   eslint: {
